04977b4f4f6bc55d2e3cacdcb9f0f4997639bafe
JanGospodarek <jan.gospo@onet.pl>
2022-10-12 10:51:37 +0200

    config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+import axios from "axios";
+import { API_KEY } from "./config/API_KEY";
+const form = document.querySelector("form")!;
+const addressInput = <HTMLInputElement>document.getElementById("address")!;
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=
+  ${API_KEY}`
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log(err));
+}
+form.addEventListener("submit", searchAddressHandler);
